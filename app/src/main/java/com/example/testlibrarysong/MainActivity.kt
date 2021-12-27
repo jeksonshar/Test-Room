43145a0dc23c771bc53ca8641ab5fda1253b99e3
JeksonShar <dev.srx981@gmail.com>
2021-12-28 00:27:10 +0200

    created spinner dropdown to open fragments with tasks

--- app/src/main/java/com/example/testlibrarysong/MainActivity.kt
+++ app/src/main/java/com/example/testlibrarysong/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.testlibrarysong
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import com.example.testlibrarysong.presentation.ui.users.UserListFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +20,28 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.fragmentContainer, UserListFragment.newInstance())
                 .commit()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val spinner: Spinner = findViewById(R.id.spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.fragments,
+            android.R.layout.simple_spinner_dropdown_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+        spinner.onItemSelectedListener = this
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        val choose = resources.getStringArray(R.array.fragments)
+        Toast.makeText(this, choose[p2].toString(), Toast.LENGTH_LONG).show()
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
     }
 }
